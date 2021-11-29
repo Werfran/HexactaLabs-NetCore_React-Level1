67a8315f60a93b5a07da64da60c3371d359b67cb
Werfran <franciscozufiaurre@gmail.com>
2021-11-29 01:13:19 -0300

    casi funciona, hay que revisar

--- Stock.Web/client-app/src/index.js
+++ Stock.Web/client-app/src/index.js
@@ -38,12 +38,13 @@ const menu_factory = (title, to, icon, children = []) => ({
   title,
   to,
   icon,
-  children
+  children,
 });
 
 const menu = [
   menu_factory("Proveedor", "/provider", "img_provider"),
-  menu_factory("Tiendas", "/store", "img_store")
+  menu_factory("Productos", "/productType", "img_provider"),
+  menu_factory("Tiendas", "/store", "img_store"),
 ];
 
 ReactDOM.render(
